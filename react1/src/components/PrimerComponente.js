092476553cd4781d5257010b533efbbdaeebe920
Joab Ramos <joab.ramos27@gmail.com>
2023-04-19 22:13:36 -0600

    modificacion de primer componente

--- react1/src/components/PrimerComponente.js
+++ react1/src/components/PrimerComponente.js
@@ -4,12 +4,28 @@ export const PrimerComponente = () => {
 
     let nombre = "Joab"
     let web = "github.com"
+    let cursos = [
+      "Master en JavaScript",
+      "Master en PHP",
+      "Master en HTML",
+      "Master en CSS",
+    ];
+
   return (
     <div>
         <h1>Mi primer componente.</h1>
         <p>Este es un texto en mi componente.</p>
         <p>Mi nobre es: {nombre}</p>
         <p>Mi sitio web es: {web}</p>
+
+        <h2>Cursos</h2>
+        <ul>
+          {cursos.map(curso => (
+            <li key={curso}>{curso}</li>
+          ))}
+        </ul>
+
+
     </div>
     
   )
